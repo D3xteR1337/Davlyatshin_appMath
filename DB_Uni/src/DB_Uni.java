63112f0bd899f5ca245ba364d516dc2c99c27a53
D3xteR1337 <ememm1236@gmail.com>
2019-10-26 14:50:03 +0300

    Final version of Practice6 with db usage

--- DB_Uni/src/DB_Uni.java
+++ DB_Uni/src/DB_Uni.java
@@ -1,18 +1,32 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 
 public class DB_Uni {
-    //  Database credentials
     static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/DB_studying";
     static final String USER = "postgres";
     static final String PASS = "83926110";
+    static private Scanner sc = new Scanner(System.in);
+    static Connection connection = null;
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws SQLException {
+        String name, country, city, adress, phone, email;
+        System.out.print("Введите имя: ");
+        name = sc.nextLine();
+        System.out.print("Введите страну: ");
+        country = sc.nextLine();
+        System.out.print("Введите город: ");
+        city = sc.nextLine();
+        System.out.print("Введите адрес: ");
+        adress = sc.nextLine();
+        System.out.print("Введите номер телефона: ");
+        phone = sc.nextLine();
+        System.out.print("Введите email: ");
+        email = sc.nextLine();
+
+
 
         System.out.println("Testing connection to PostgreSQL JDBC");
-
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -22,12 +36,10 @@ public class DB_Uni {
         }
 
         System.out.println("PostgreSQL JDBC Driver successfully connected");
-        Connection connection = null;
+
 
         try {
-            connection = DriverManager
-                    .getConnection(DB_URL, USER, PASS);
-
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             e.printStackTrace();
@@ -40,5 +52,36 @@ public class DB_Uni {
             System.out.println("Failed to make connection to database");
         }
 
+        Statement statement = connection.createStatement();
+
+        statement.executeUpdate("INSERT INTO db_in VALUES ('ООО Шуба','Россия','Чебоксары','ул. Петрова, д.4','237-89-08','mex@mail.ru')");
+        print_table();
+        statement.executeUpdate("INSERT INTO db_in VALUES ('"+(name) +"','"+country+"','"+city+"','"+adress+"','"+phone+"','"+email+"')");
+        print_table();
+        statement.executeUpdate("UPDATE db_in SET clientname = 'Институт ядерных технологий' WHERE clientname = 'НИИ Атоммашстрой'");
+        print_table();
+        statement.executeUpdate("DELETE FROM db_in WHERE clientname = 'Левачев Николай'");
+        print_table();
+
+        statement.close();
+        connection.close();
+
+    }
+
+    public static void print_table() throws SQLException {
+        Statement st = connection.createStatement();
+        ResultSet rsName = st.executeQuery("SELECT * FROM db_in");
+        ResultSetMetaData rsmdName = rsName.getMetaData();
+
+        int columnsNumber = rsmdName.getColumnCount();
+
+        while(rsName.next()){
+            for(int i = 1; i <= columnsNumber; i++){
+                System.out.print(rsName.getString(i)+"| ");
+            }
+            System.out.println();
+        }
+        st.close();
+        rsName.close();
     }
 }
